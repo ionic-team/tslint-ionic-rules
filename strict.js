8bfdc273b285f9453ce847c0e8e930c20fb64992
Daniel Imhoff <dwieeb@gmail.com>
2018-06-08 13:27:18 -0500

    feat: strict ordered imports

--- strict.js
+++ strict.js
@@ -11,7 +11,7 @@ module.exports = {
     'arrow-parens': [true, 'ban-single-arg-parens'],
     'one-variable-per-declaration': true,
     'trailing-comma': [true, { 'multiline': { 'objects': 'always', 'arrays': 'always', 'functions': 'never', 'typeLiterals': 'always' }, 'singleline': 'never' }],
-    // 'ordered-imports': { 'options': { 'grouped-imports': true, 'import-sources-order': 'lowercase-last', 'named-imports-order': 'lowercase-last' } },
+    'ordered-imports': { 'options': { 'grouped-imports': true, 'import-sources-order': 'lowercase-last', 'named-imports-order': 'lowercase-last' } },
 
     // Overridden from default rules that were re-accepted from recommended (see
     // https://github.com/palantir/tslint/blob/2b8a7374cf0fa28e85d95beb40aafd7cb011d157/src/configs/recommended.ts#L18-L204)
